72475f4a588de309ffc2db60d0bb52d489f11251
kushal <kushaldeveloper@gmail.com>
2020-10-01 17:28:53 -0600

    add , {"waitUntil" : "networkidle0"}
    from https://stackoverflow.com/a/46217285/227646

--- index.js
+++ index.js
@@ -12,44 +12,44 @@ const run = async () => {
       height: 1080
   })
 
-  await page.goto('https://slickdeals.net/')
+  await page.goto('https://slickdeals.net/', {"waitUntil" : "networkidle0"})
   await page.screenshot({
       path: 'slickdeals-frontpage.png',
       fullPage: true
   })
 
-  await page.goto('https://slickdeals.net/deals/')
+  await page.goto('https://slickdeals.net/deals/', {"waitUntil" : "networkidle0"})
   await page.screenshot({
       path: 'slickdeals-new.png',
       fullPage: true
   })
 
-  await page.goto('https://news.ycombinator.com/newest')
+  await page.goto('https://news.ycombinator.com/newest', {"waitUntil" : "networkidle0"})
   await page.screenshot({
       path: 'hackernews-new.png',
       fullPage: true
   })
 
 
-  await page.goto('https://old.reddit.com/user/7165015874/m/buy/new/')
+  await page.goto('https://old.reddit.com/user/7165015874/m/buy/new/', {"waitUntil" : "networkidle0"})
   await page.screenshot({
       path: 'reddit-hardwareswap-new.png',
       fullPage: true
   })
 
-  await page.goto('https://www.amazon.com/gp/goldbox')
+  await page.goto('https://www.amazon.com/gp/goldbox', {"waitUntil" : "networkidle0"})
   await page.screenshot({
       path: 'amazon-goldbox.png',
       fullPage: true
   })
 
-  await page.goto('https://www.amazon.com/best-sellers-camera-photo/zgbs/photo/')
+  await page.goto('https://www.amazon.com/best-sellers-camera-photo/zgbs/photo/', {"waitUntil" : "networkidle0"})
   await page.screenshot({
       path: 'amazon-bestsellers-camera.png',
       fullPage: true
   })
 
-  await page.goto('https://www.amazon.com/Best-Sellers-Electronics/zgbs/electronics/')
+  await page.goto('https://www.amazon.com/Best-Sellers-Electronics/zgbs/electronics/', {"waitUntil" : "networkidle0"})
   await page.screenshot({
       path: 'amazon-bestsellers-electronics.png',
       fullPage: true
