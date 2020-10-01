0c3e9af8315214c3568ad9de8c587ef317a12dc8
kushal <kushaldeveloper@gmail.com>
2020-10-01 16:22:35 -0600

    learn puppeteer
    [kushal@ideapadflex-kushal html]$ mkdir mypuppeteer
    [kushal@ideapadflex-kushal html]$ cd mypuppeteer/
    [kushal@ideapadflex-kushal mypuppeteer]$ time yarn add puppeteer
    yarn add v1.22.5
    info No lockfile found.
    [1/4] Resolving packages...
    [2/4] Fetching packages...
    [3/4] Linking dependencies...
    [4/4] Building fresh packages...
    success Saved lockfile.
    success Saved 50 new dependencies.
    info Direct dependencies
    └─ puppeteer@5.3.1
    info All dependencies
    ├─ @types/node@14.11.2
    ├─ @types/yauzl@2.9.1
    ├─ agent-base@5.1.1
    ├─ balanced-match@1.0.0
    ├─ base64-js@1.3.1
    ├─ bl@4.0.3
    ├─ brace-expansion@1.1.11
    ├─ buffer-crc32@0.2.13
    ├─ buffer@5.6.0
    ├─ chownr@1.1.4
    ├─ concat-map@0.0.1
    ├─ debug@4.2.0
    ├─ devtools-protocol@0.0.799653
    ├─ end-of-stream@1.4.4
    ├─ extract-zip@2.0.1
    ├─ fd-slicer@1.1.0
    ├─ find-up@4.1.0
    ├─ fs-constants@1.0.0
    ├─ fs.realpath@1.0.0
    ├─ get-stream@5.2.0
    ├─ glob@7.1.6
    ├─ https-proxy-agent@4.0.0
    ├─ ieee754@1.1.13
    ├─ inflight@1.0.6
    ├─ locate-path@5.0.0
    ├─ minimatch@3.0.4
    ├─ mkdirp-classic@0.5.3
    ├─ ms@2.1.2
    ├─ once@1.4.0
    ├─ p-limit@2.3.0
    ├─ p-locate@4.1.0
    ├─ p-try@2.2.0
    ├─ path-exists@4.0.0
    ├─ path-is-absolute@1.0.1
    ├─ pend@1.2.0
    ├─ pkg-dir@4.2.0
    ├─ progress@2.0.3
    ├─ proxy-from-env@1.1.0
    ├─ puppeteer@5.3.1
    ├─ readable-stream@3.6.0
    ├─ rimraf@3.0.2
    ├─ safe-buffer@5.2.1
    ├─ string_decoder@1.3.0
    ├─ tar-fs@2.1.0
    ├─ tar-stream@2.1.4
    ├─ through@2.3.8
    ├─ unbzip2-stream@1.4.3
    ├─ util-deprecate@1.0.2
    ├─ ws@7.3.1
    └─ yauzl@2.10.0
    Done in 61.39s.
    
    real    1m1.669s
    user    0m10.545s
    sys     0m3.394s
    [kushal@ideapadflex-kushal mypuppeteer]$

---
+++ index.js
@@ -0,0 +1,26 @@
+const puppeteer = require("puppeteer");
+
+// we're using async/await - so we need an async function, that we can run
+const run = async () => {
+  // open the browser and prepare a page
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  // set the size of the viewport, so our screenshot will have the desired size
+  await page.setViewport({
+      width: 1920,
+      height: 1080
+  })
+
+  await page.goto('https://www.slickdeals.net/deals')
+  await page.screenshot({
+      path: 'deals.png',
+      fullPage: true
+  })
+
+  // close the browser 
+  await browser.close();
+};
+
+// run the async function
+run();
